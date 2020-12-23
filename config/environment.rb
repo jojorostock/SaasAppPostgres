2a308ff82df913fd3d8dce6177b64b190dcf57c6
Jojo Rostock <jojorostock@yahoo.com>
2020-12-23 09:07:17 -0500

    trying other environment set ups

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,6 @@
 # Load the Rails application.
+require File.expand_path('../application', __FILE__)
+# Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
